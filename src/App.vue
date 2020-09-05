601d03d165f126c95f0119edb88f8e473e7739ec
arathi guedes <a194330@dac.unicamp.br>
2020-09-05 10:10:52 -0300

    beto img

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Beto" src="./assets/beto.png">
+    <HelloWorld msg="Welcome to BetoVue.js App"/>
   </div>
 </template>
 
